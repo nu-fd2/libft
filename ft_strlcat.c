c497963d2726da4d4ae74a78384f9f63ae4fdbcc
fd2 <omar0madouni@gmail.com>
2024-11-15 16:35:36 +0100

    a

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: oel-mado <oel-mado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 02:56:44 by oel-mado          #+#    #+#             */
-/*   Updated: 2024/11/14 21:41:49 by oel-mado         ###   ########.fr       */
+/*   Updated: 2024/11/15 16:12:15 by oel-mado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t n)
 	i = 0;
 	j = 0;
 	if (!dst && !n)
-		return (0);
+		return (ft_strlen(src));
 	k = ft_strlen((const char *)dst);
 	if (n < ft_strlen(dst))
 		return (ft_strlen(src) + n);
